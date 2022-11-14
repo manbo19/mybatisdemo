ac851f3cd6b537ddacaf85d5259413f8f981ecf9
jeongmin <jeongmin0619@gmail.com>
2022-11-14 18:01:25 +0900

    20111114

---
+++ src/part01/controller/EmpController.java
@@ -0,0 +1,28 @@
+package part01.controller;
+
+import java.util.List;
+
+import part01.dao.EmployeesDAO;
+import part01.dto.EmpDTO;
+
+public class EmpController {
+
+	private EmployeesDAO dao;
+
+	public EmpController() {
+		dao = new EmployeesDAO();
+	}
+
+	public int countProcess() {
+		return dao.countMethod();
+	}
+
+	public List<EmpDTO> listProcess() {
+		return dao.listMethod();
+	}
+
+	public List<EmpDTO> searchProcess(String Data) {
+		return dao.searchMethod(Data);
+	}
+
+}
---
+++ src/part02/controller/MemController.java
@@ -0,0 +1,33 @@
+package part02.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import part02.dao.MemDAO;
+import part02.dto.MemDTO;
+
+public class MemController {
+
+	private MemDAO dao;
+
+	public MemController() {
+		dao = new MemDAO();
+	}
+
+	public List<MemDTO> listProcess() {
+		return dao.listMethod();
+	}// end listProcess()
+
+	public int insertProcess(MemDTO dto) {
+		return dao.insertMethod(dto);
+	}// end insertProcess()
+
+	public int updateProcess(HashMap<String, Object> hmap) {
+		return dao.updateMethod(hmap);
+	}// end updateProcess()
+
+	public int deleteProcess(int age) {
+		return dao.deleteMethod(age);
+	}// end deleteProcess()
+
+}// end class
